47c9530fbb3ac20d1d2beb4c7b5cbfcda7701055
Daniel Robinson <daniel.robinson.open@gmail.com>
2014-04-14 22:35:30 +0100

    fix client side bug
    Purity client core expects a Stream<String> not a Stream<MessageEvent>

--- lib/src/client/purity_client.dart
+++ lib/src/client/purity_client.dart
@@ -13,5 +13,5 @@ void initPurityAppView(String protocol, InitAppView initAppView, OnConnectionClo
   _hasInitialised = true;
   var ws = new WebSocket('$protocol://${Uri.base.host}:${Uri.base.port}$PURITY_SOCKET_ROUTE_PATH');
   ws.onOpen.first.then((_) =>
-    new PurityClientCore(initAppView, onConnectionClose, ws.onMessage, ws.sendString));
+    new PurityClientCore(initAppView, onConnectionClose, ws.onMessage.map((msg) => msg.data), ws.sendString));
 }
