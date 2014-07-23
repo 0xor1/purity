11115096f30f6cce99182b350c17ec926d09ce2e
0xor1 <daniel.robinson.open@gmail.com>
2014-07-23 22:26:31 +0100

    fix indentation

--- lib/host.dart
+++ lib/host.dart
@@ -29,45 +29,45 @@ class Host extends core.Host{
 
     HttpServer.bind(address, port).then((server) {
 
-        _log.info("Server is running on "
-                 "'http://${server.address.address}:$port/'");
+      _log.info("Server is running on "
+               "'http://${server.address.address}:$port/'");
 
-        var router = new Router(server);
+      var router = new Router(server);
 
-        router.serve(core.PURITY_WEB_SOCKET_ROUTE_PATH).listen((HttpRequest request){
-          if(WebSocketTransformer.isUpgradeRequest(request)){
-            WebSocketTransformer.upgrade(request)
-            .then((ws){
-              var biConnection = new core.EndPointConnection(ws, ws.add, ws.close);
-              createSourceEndPoint(request.connectionInfo.remoteAddress.toString(), biConnection);
-            });
-          }else{
-            _log.warning("Purity app web socket request not valid");
-            request.response.statusCode = HttpStatus.BAD_REQUEST;
-            request.response.close();
-          }
-        });
-
-        // Default handler serves files.
-        var virDir = new http_server.VirtualDirectory(staticFileDirectory);
-        virDir.jailRoot = true;
-        virDir.allowDirectoryListing = true;
-        virDir.directoryHandler = (dir, request) {
-          // Redirect directory requests to index.html files.
-          var indexUri = new Uri.file(dir.path).resolve('index.html');
-          virDir.serveFile(new File(indexUri.toFilePath()), request);
-        };
-
-        // Add an error page handler.
-        virDir.errorPageHandler = (HttpRequest request) {
-          _log.warning("Resource not found: ${request.uri.path}");
-          request.response.statusCode = HttpStatus.NOT_FOUND;
+      router.serve(core.PURITY_WEB_SOCKET_ROUTE_PATH).listen((HttpRequest request){
+        if(WebSocketTransformer.isUpgradeRequest(request)){
+          WebSocketTransformer.upgrade(request)
+          .then((ws){
+            var biConnection = new core.EndPointConnection(ws, ws.add, ws.close);
+            createSourceEndPoint(request.connectionInfo.remoteAddress.toString(), biConnection);
+          });
+        }else{
+          _log.warning("Purity app web socket request not valid");
+          request.response.statusCode = HttpStatus.BAD_REQUEST;
           request.response.close();
-        };
-
-        // Serve everything not routed elsewhere through the virtual directory.
-        virDir.serve(router.defaultStream);
+        }
       });
+
+      // Default handler serves files.
+      var virDir = new http_server.VirtualDirectory(staticFileDirectory);
+      virDir.jailRoot = true;
+      virDir.allowDirectoryListing = true;
+      virDir.directoryHandler = (dir, request) {
+        // Redirect directory requests to index.html files.
+        var indexUri = new Uri.file(dir.path).resolve('index.html');
+        virDir.serveFile(new File(indexUri.toFilePath()), request);
+      };
+
+      // Add an error page handler.
+      virDir.errorPageHandler = (HttpRequest request) {
+        _log.warning("Resource not found: ${request.uri.path}");
+        request.response.statusCode = HttpStatus.NOT_FOUND;
+        request.response.close();
+      };
+
+      // Serve everything not routed elsewhere through the virtual directory.
+      virDir.serve(router.defaultStream);
+    });
 
   }
 }
